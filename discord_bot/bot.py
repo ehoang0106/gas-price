0633d41f43df538c4eef12009d3ce55a0fb4b39f
Khoa Hoang <khoahoang1698@gmail.com>
2024-10-30 15:21:48 -0700

    fix code for bot

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -16,6 +16,15 @@ WORKFLOW_ID = 'workflow.yml'
 bot = discord.Client()
 bot = commands.Bot(command_prefix=PREFIX)
 
+#setup gihub api
+
+url = f'https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches'
+headers = { 'Authorization': f'token {GITHUB_TOKEN}' }
+payload = { 'ref': 'master' }
+response = requests.post(url, headers=headers, json=payload)
+
+#end setup github api
+
 
 @bot.event
 async def on_ready():
@@ -23,27 +32,18 @@ async def on_ready():
   await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="gas prices"))
 
 @bot.command(name="gas")
-async def gas(ctx):
-  await ctx.send("I'm searching for gas prices Garden Grove, CA.\n Please wait a moment...")
-  url = f'https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches'
-  
-  headers = { 'Authorization': f'token {GITHUB_TOKEN}' }
-  payload = { 'ref': 'master' }
-  
-  response = requests.post(url, headers=headers, json=payload)
+async def gas(ctx, *, location):
+  await ctx.send(f"I'm searching for gas prices {location}.\n Please wait a moment...")
   
   if response.status_code == 204:
-    location = "Garden Grove, CA"
     gas_prices = search_gas_prices(location)
     if gas_prices:
         print('Results: ')
         for station in gas_prices:
           print(station)
-          await ctx.send(f"Station Name: {station['station_name']}\nPrice: {station['price']}\nAddress: {station['address']}\n-------------------")
+          await ctx.send(f"```Station Name: {station['station_name']}\nPrice: {station['price']}\nAddress: {station['address']}\n-------------------```")
     else:
       await ctx.send("An error occurred while searching for gas prices.")
-    
-
   
   
 bot.run(TOKEN)
