1711e581168af464b7d6f694fb53623eb2bcb70e
Khoa Hoang <khoahoang1698@gmail.com>
2024-10-31 15:50:50 -0700

    terraform and congifure terraform in workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,26 @@ resource "aws_dynamodb_table" "GasPrices" {
   }
 
 }
+
+resource "aws_cloudwatch_metric_alarm" "dynamodb_write_capacity_alarm" {
+  alarm_name          = "DynamoDBWriteCapacityAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ConsumedWriteCapacityUnits"
+  namespace           = "AWS/DynamoDB"
+  period              = 43200  #12 hours
+  statistic           = "Sum"
+  threshold           = 5
+  alarm_description   = "Alarm when write capacity units exceed 5 in 12 hours"
+  dimensions = {
+    TableName = aws_dynamodb_table.GasPricesTracker.name
+  }
+
+  alarm_actions = [
+    aws_sns_topic.DataWriteInDynamoDB.arn
+  ]
+}
+
+resource "aws_sns_topic" "DataWriteInDynamoDB" {
+  name = "DataWriteInDynamoDB"
+}
