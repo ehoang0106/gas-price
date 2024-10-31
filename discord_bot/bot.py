7c7a1cb0e037e03fca3906f20bfacc44bde4c385
Khoa Hoang <khoahoang1698@gmail.com>
2024-10-31 14:59:46 -0700

    remove trigger github workflow since its not a case anymore :(

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -15,13 +15,14 @@ bot = commands.Bot(command_prefix=PREFIX)
 
 #setup gihub api
 #this setup is to trigger the github action to run the gas price search
-GITHUB_TOKEN = config['GITHUB_TOKEN']
-OWNER = 'ehoang0106'
-REPO = 'gas-price'
-WORKFLOW_ID = 'workflow.yml'
-url = f'https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches'
-headers = { 'Authorization': f'token {GITHUB_TOKEN}' }
-payload = { 'ref': 'master' }
+
+# GITHUB_TOKEN = config['GITHUB_TOKEN']
+# OWNER = 'ehoang0106'
+# REPO = 'gas-price'
+# WORKFLOW_ID = 'workflow.yml'
+# url = f'https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches'
+# headers = { 'Authorization': f'token {GITHUB_TOKEN}' }
+# payload = { 'ref': 'master' }
 
 #end setup github api
 
@@ -36,26 +37,26 @@ async def gas(ctx, *, location): # * is get all the arguments after the command
   await ctx.send(f"I'm searching for gas prices in {location}.\n Please wait a moment...\n")
   
   #call api to trigger the github action
-  response = requests.post(url, headers=headers, json=payload)
+  #response = requests.post(url, headers=headers, json=payload)
   
-  if response.status_code == 204:
-    gas_prices = search_gas_prices(location)
-    if gas_prices:
-        print('Results: ')
-        await ctx.send(f"✅ Here are the gas stations near **{location}** I found.\n")
-        for station in gas_prices:
-          print(station)
-          
-          await ctx.send("\n**--Gas Station--**\n")
-          await ctx.send(f"```---------------\n⛽ Station Name: {station['station_name']}\n💵 Price: {station['price']}\n🗺️ Address: {station['address']}\n```\n{station['map_url']}\n")
-    else:
-      await ctx.send("An error occurred while searching for gas prices.")
-      
-    await ctx.send("\n-----------------------------------------\n🖥️ Triggering GitHub Action to spread into DynamoDB...")
-    time.sleep(5)
-    await ctx.send("✅ GitHub Action also completed successfully.")
+  #if response.status_code == 204:
+  gas_prices = search_gas_prices(location)
+  if gas_prices:
+      print('Results: ')
+      await ctx.send(f"✅ Here are the gas stations near **{location}** I found.\n")
+      for station in gas_prices:
+        print(station)
+        
+        await ctx.send("\n**--Gas Station--**\n")
+        await ctx.send(f"```---------------\n⛽ Station Name: {station['station_name']}\n💵 Price: {station['price']}\n🗺️ Address: {station['address']}\n```\n{station['map_url']}\n")
   else:
-    await ctx.send("❌ GitHub Action failed to run.") 
+    await ctx.send("An error occurred while searching for gas prices.")
+    
+  await ctx.send("\n-----------------------------------------\n🖥️ Triggering GitHub Action to spread into DynamoDB...")
+  time.sleep(5)
+  await ctx.send("✅ GitHub Action also completed successfully.")
+  # else:
+  #   await ctx.send("❌ GitHub Action failed to run.") 
   
   
 bot.run(TOKEN)
