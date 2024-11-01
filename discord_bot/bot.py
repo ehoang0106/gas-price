6dcf4550abd324aff3a6d74b28f221cf20a60a54
Khoa Hoang <khoahoang1698@gmail.com>
2024-11-01 11:18:50 -0700

    update...

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -33,15 +33,15 @@ async def on_ready():
   await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="gas prices"))
 
 @bot.command(name="gas")
-async def gas(ctx, *, location): # * is get all the arguments after the command
-  await ctx.send(f"I'm searching for gas prices in {location}.\nPlease wait a moment...\n")
+async def gas(ctx): # * is get all the arguments after the command
+  await ctx.send(f"I'm searching for gas prices.\nPlease wait a moment...\n")
   
   #call api to trigger the github action
   #response = requests.post(url, headers=headers, json=payload)
   
   #if response.status_code == 204:
-  gas_prices, count = search_gas_prices(location)
-  await ctx.send(f"\n✅ Here are ***`{count}`*** the gas stations near **{location}** that I found.\n")
+  gas_prices, count = search_gas_prices("Garden Grove")
+  await ctx.send(f"\n✅ Here are **`{count}`** the gas stations near you that I found.\n")
   if gas_prices:
       print('Results: ')
       for station in gas_prices:
