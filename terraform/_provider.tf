e34875fb21587a5f7c176e1f521de57c1626f92b
ehoang0106 <khoahoang1698@gmail.com>
2024-11-04 12:00:01 -0800

    add s3 resource in tf

--- terraform/_provider.tf
+++ terraform/_provider.tf
@@ -5,11 +5,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "terraform_state_bucket"
-    key = "terraform_state_for_gasprices"
-    region = "us-west-1"
-  }
+  # backend "s3" {
+  #   bucket = "terraform_state_bucket"
+  #   key = "terraform_state_for_gasprices"
+  #   region = "us-west-1"
+  # }
 }
 
 provider "aws" {
