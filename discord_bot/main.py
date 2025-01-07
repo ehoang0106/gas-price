778212ce6722980b8f8629f141ea36e89d7b53be
Khoa Hoang <khoahoang1698@gmail.com>
2025-01-07 12:15:01 -0800

    edit dynamodb table name

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -24,7 +24,7 @@ def init_driver():
 
 def insert_into_dynamodb(date, station_name, price, gas_type, address):
     dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
-    table = dynamodb.Table('GasPricesTracker')
+    table = dynamodb.Table('GasPricesTracking')
     
     response = table.put_item(
         Item={
